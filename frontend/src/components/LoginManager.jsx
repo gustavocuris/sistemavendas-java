699347f274cc67022c0dfd5edaae877c463f5846
Gustavo Curis <gustavo.c.francisco2007@gmail.com>
2026-03-11 13:01:05 -0300

    style: remove extra stroke from edit pencil icon

--- frontend/src/components/LoginManager.jsx
+++ frontend/src/components/LoginManager.jsx
@@ -154,7 +154,6 @@ export default function LoginManager({
                           title="Editar conta"
                         >
                           <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-                            <path d="M12 20h9"></path>
                             <path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19.5 2 21l1.5-5L16.5 3.5z"></path>
                           </svg>
                         </button>
